3964de11a567c8d9a187efb9d5f50d2a7b1a5f6d
Scott Cote <cote.scott.f@gmail.com>
2016-04-26 19:37:13 -0400

    Added parsergen

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var through = require('through2');
+var through2 = require('through2');
+var ReadableStreamBuffer = require('stream-buffers').ReadableStreamBuffer;
+
+var generator = require('parsergen').default;
 
 var PLUGIN_NAME = 'gulp-parsergen';
 
-var doSomethingWithTheFile = function(file) {
-  return file;
-};
+module.exports = function(options) {
+  return through2.obj(function(file, encoding, done) {
 
-module.exports = function() {
-  return through.obj(function(file, encoding, callback) {
-    callback(null, doSomethingWithTheFile(file));
+    if (file.isBuffer()) {
+      var stream = new ReadableStreamBuffer();
+      stream.put(file.contents)
+      stream.stop();
+      stream
+        .pipe(generator(options))
+        .pipe(through2(function(chunk, enc, done) {
+          file.contents = chunk;
+        }));
+    } else if (file.isStream()) {
+      file.contents = file.contents.pipe(generator(options));
+    }
+
+    done(null, file);
   });
 };
-
